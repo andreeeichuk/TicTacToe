44c5d1f3c62cb9a5b9d39a1d9c5082ea05c81140
Ivan Andreichuk <andreeeichuk@gmail.com>
2019-09-07 14:51:16 +0300

    add game singleton

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    private static Game instance;
+
+    public static Game Instance
+    {
+        get
+        {
+            if(instance==null)
+            {
+                GameObject singletonGO = new GameObject();
+                instance = singletonGO.AddComponent<Game>();
+                singletonGO.name = typeof(Game).ToString() + " (Singleton)";
+                DontDestroyOnLoad(singletonGO);
+            }
+
+            return instance;
+        }        
+    }
+}
